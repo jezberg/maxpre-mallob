f73132d80e943b69c85c9871f8ae248206bddc53
Hansuzu <my.address@gmail.com>
2022-06-06 09:12:09 +0300

    maxpre2

---
+++ src/satsolver/solvers/glucose-3.0/core/depend.mk
@@ -0,0 +1,53 @@
+/Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Main.o /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Main.or /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Main.od /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Main.op: \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Main.cc \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/System.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/ParseUtils.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/Options.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/../mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/../mtl/XAlloc.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../utils/ParseUtils.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/Dimacs.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/SolverTypes.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Alg.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Map.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/../mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Alloc.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/../mtl/XAlloc.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/Solver.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Heap.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Alg.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/BoundedQueue.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/Constants.h
+/Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Solver.o /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Solver.or /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Solver.od /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Solver.op: \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/Solver.cc \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Sort.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/../mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/../mtl/XAlloc.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/Solver.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/SolverTypes.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Alg.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Map.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/../mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/Alloc.h \
+  /Users/jezberg/Documents/Research-repositories/loandra/solver/code/maxpre/satsolver/solvers/glucose-3.0/mtl/../mtl/XAlloc.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Heap.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../mtl/Alg.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/Options.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/IntTypes.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../mtl/Vec.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/../utils/ParseUtils.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/BoundedQueue.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../core/Constants.h \
+  /Users/jezberg/Documents/Research-repositories/maxpre/src/satsolver/solvers/glucose-3.0/core/../utils/System.h
